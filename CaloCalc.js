17505b2c9dfbb73c12d0fcc4a81b599606e9122c
alotofeve <68940442+alotofeve@users.noreply.github.com>
2021-04-12 10:13:02 -0700

    Update CaloCalc.js

--- CaloCalc.js
+++ CaloCalc.js
@@ -1,102 +1,106 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
     //variables
     var selectedFruit = {
         fruit: "(None)",
         ServingSize: "(None)",
-        ServingCalo: 0
+        ServingCalo: 0,
     };
 
     var selectedServ = 0;
 
-    var Fruits = [{
-            fruit: "Apple",
-            SS: "1 large (242g)",
-            SC: 100
-        },
-        {
-            fruit: "Avocado",
-            SS: "1 medium (150g)",
-            SC: 250
-        },
-        {
-            fruit: "Lemon",
-            SS: "1 medium (58g)",
-            SC: 15
-        },
-        {
-            fruit: "Peach",
-            SS: "1 medium (147g)",
-            SC: 60
-        },
-        {
-            fruit: "Cherries",
-            SS: "21 cherries (140g)",
-            SC: 100
-        }
-    ]
+    var Fruits = [
+        { fruit: "FruitArray", SS: " ", SC: 0 },
+        { fruit: "Apple", SS: "1 large (242g)", SC: 100 },
+        { fruit: "Avocado", SS: "1 medium (150g)", SC: 250 },
+        { fruit: "Lemon", SS: "1 medium (58g)", SC: 15 },
+        { fruit: "Peach", SS: "1 medium (147g)", SC: 60 },
+        { fruit: "Cherries", SS: "21 cherries (140g)", SC: 100 },
+        { fruit: "Pear", SS: "1 medium (166g)", SC: 100 },
+    ];
 
-    console.log("Fruit Name:" + Fruits.length);
-    for (let i = 0; i < Fruits.length - 1; i++) {
-        console.log("Fruits:" + Fruits[i].fruit);
+    //check if there new item in Fruit Array
+    var selFruitArray = Array.from(
+        document.getElementById("selFruit").options
+    ).map((e) => e.value);
+
+    var sel = document.getElementById("selFruit");
+    var newFruit = document.createElement("option");
+    if (Fruits.length > selFruitArray.length) {
+        let dif = Fruits.length - selFruitArray.length + 1;
+        for (let i = 0; i < dif; i++) {
+            for (let j = i; j < selFruitArray.length + i; j++) {
+                if (selFruitArray.indexOf(Fruits[j].fruit) === -1) {
+                    newFruit.value = Fruits[j].fruit;
+                    newFruit.text = Fruits[j].fruit;
+                    sel.add(newFruit, null);
+                    console.log("add one new fruit");
+                }
+            }
+        }
     }
 
     var fruit_SC = 0;
     let CaloTaken = 0;
     var Frecord = ["fruits selected:"];
     const limitCalo = 500;
+    var select1;
+    var selIndex;
 
-    $('select').on('change', function() {
-            var selIndex = parseInt(document.getElementById("selFruit").value);
-            var select1 = Fruits[selIndex].fruit;
-            console.log(select1);
+    // $('select').on('change', function() {
+    document.getElementById(`selFruit`).addEventListener(`change`, function() {
+        selIndex = document.getElementById("selFruit").selectedIndex;
+        select1 = Fruits[selIndex].fruit;
+        fruit_SC = Fruits[selIndex].SC;
 
-            $("#selectedFruit").html(selectedFruitFnc(Fruits[selIndex].fruit, Fruits[selIndex].SS, Fruits[selIndex].SC));
-            fruit_SC = Fruits[selIndex].SC;
-            console.log(fruit_SC);
+        selectedFruit.fruit = Fruits[selIndex].fruit;
+        selectedFruit.ServingSize = Fruits[selIndex].SS;
+        selectedFruit.ServingCalo = Fruits[selIndex].SC;
+        document.getElementById("selectedFruit").innerHTML =
+            Fruits[selIndex].fruit +
+            ": Calories:" +
+            Fruits[selIndex].SC +
+            " per " +
+            Fruits[selIndex].SS;
 
-            document.getElementById("Estimate").onclick = function() {
-                EstiCalo(fruit_SC);
-            }
-            document.getElementById("Confirm").onclick = function() {
-                ConfirmSel(select1, Frecord, fruit_SC, selectedServ);
-            };
+        document.getElementById("Estimate").onclick = function() {
+            EstiCalo(fruit_SC);
+        };
+        document.getElementById("Confirm").onclick = function() {
+            ConfirmSel(select1, Frecord, fruit_SC, selectedServ);
+        };
+    });
 
-            // console.log("Fruit length:"+Frecord.length);           
-
-        })
-        //      } while(i<3);
     function EstiCalo(fruit_SC) {
         selectedServ = document.getElementById("selServing").value;
         var EstTaken = fruit_SC * selectedServ;
-        document.getElementById("EstTaken").innerHTML = "Estimate Calorise: " + EstTaken;
+        document.getElementById("EstTaken").innerHTML = EstTaken;
         var EstTotal = EstTaken + CaloTaken;
         if (EstTotal > limitCalo) {
-            alert("You will take too much calorise! Please re-choose");
+            alert("You taken too much calorise! Please change");
             return;
         }
     }
 
-    function selectedFruitFnc(fruit, ServingSize, ServingCalo) {
-        selectedFruit.fruit = fruit;
-        selectedFruit.ServingSize = ServingSize;
-        selectedFruit.ServingCalo = ServingCalo;
-        return fruit + ": Calories:" + ServingCalo + " per " + ServingSize;
-    }
-
     function ConfirmSel(select1, Frecord, fruit_SC, selectedServ) {
-        if (select1 === "" || fruit_SC === "" || Frecord === "" || selectedServ === 0) {
+        if (
+            select1 === "" ||
+            fruit_SC === "" ||
+            Frecord === "" ||
+            selectedServ === 0
+        ) {
             alert("Please do estimate");
             return;
         }
         Frecord.push(select1);
-        document.getElementById("HistorySel").innerHTML += Frecord[Frecord.length - 1] + ", ";
+        document.getElementById("HistorySel").innerHTML +=
+            Frecord[Frecord.length - 1] + ", ";
         // console.log(i);
         var NewTaken = fruit_SC * selectedServ;
         CaloTaken = CaloTaken + NewTaken;
 
         document.getElementById("NewTaken").innerHTML = NewTaken;
         document.getElementById("CaloTaken").innerHTML = CaloTaken;
-        document.getElementById("EstTaken").innerHTML = "Estimate Calories";
+        document.getElementById("selServing").innerHTML = " ";
+        document.getElementById("EstTaken").innerHTML = " ";
     }
-
 });
